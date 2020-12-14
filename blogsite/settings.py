ce3bb33dea34b52d7c95eaac97bee34e681a0588
AmZaDin14 <amrireza97@gmail.com>
2020-12-14 08:00:27 +0700

    change to postgresql

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'blogsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
-        # 'USER': 'amri',
-        # 'PASSWORD': 'admin',
-        # 'HOST': 'starlights.online'
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'StarLigHTs',
+        'USER': 'amri',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost'
     }
 }
 
